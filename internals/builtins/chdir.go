ed2ff2e6ee1a67af02292ab69ef43d746374bffe
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2021-02-16 18:54:55 +0100

    fix(chdir): change dir to `$HOME` if no args were given

--- internals/builtins/chdir.go
+++ internals/builtins/chdir.go
@@ -4,5 +4,10 @@ import "os"
 
 // Chdir changes the current working directory.
 func Chdir(args ...string) error {
+	if len(args) == 0 {
+		home, _ := os.UserHomeDir()
+		return os.Chdir(home)
+	}
+
 	return os.Chdir(args[0])
 }
