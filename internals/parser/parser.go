dd32a55674be2d571ef112dd2f1014973ab76dda
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2021-02-16 14:58:32 +0100

    feat(parser): expand `~` to `$HOME`

--- internals/parser/parser.go
+++ internals/parser/parser.go
@@ -6,13 +6,24 @@ import (
 	"strings"
 )
 
+func preprocessInput(input string) (output string) {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		panic(err)
+	}
+
+	output = input
+	output = strings.ReplaceAll(output, "~", home)
+	output = os.ExpandEnv(output)
+	return
+}
+
 func ParseCommand(input string) error {
 	if input == "" {
 		return nil
 	}
 
-	input = os.ExpandEnv(input)
-
+	input = preprocessInput(input)
 	cmdSplit := strings.Split(input, " ")
 
 	cmd := exec.Command(cmdSplit[0], cmdSplit[1:]...)
